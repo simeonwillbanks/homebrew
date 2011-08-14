a87934859ae44b47b8e10f1b38c5d104646cd6fd
Simeon Willbanks <sfw@simeonfosterwillbanks.com>
2011-08-14 13:37:36 -0700

    Trying to fix "‘DWORD’ has a previous declaration as ‘typedef long unsigned int DWORD’" with http://stackoverflow.com/questions/4894842/sphinx-install-error

--- Library/Formula/sphinx.rb
+++ Library/Formula/sphinx.rb
@@ -9,7 +9,7 @@ class Sphinx < Formula
   fails_with_llvm "fails with: ld: rel32 out of range in _GetPrivateProfileString from /usr/lib/libodbc.a(SQLGetPrivateProfileString.o)"
 
   def install
-    args = ["--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-mysql=/usr/local/mysql"]
+    args = ["--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-mysql=/usr/local/mysql", "--with-unixodbc=no"]
     # configure script won't auto-select PostgreSQL
     args << "--with-pgsql" if `/usr/bin/which pg_config`.size > 0
 
